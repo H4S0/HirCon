b948bd3ba495fb87b4dcd1dc9524f770b93c4d37
H4S0 <hasanalic10@gmail.com>
2024-10-04 19:55:15 +0200

    updated

--- Desktop/jobapp/app/dashboard/page.tsx
+++ Desktop/jobapp/app/dashboard/page.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const page = () => {
   return (
     <div>
-      imeee <LogoutLink>Logout</LogoutLink>
+      imeeetest <LogoutLink>Logout</LogoutLink>
     </div>
   );
 };
