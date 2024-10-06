e9191333da92c8c2767575e6669a8f7c37d6add7
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-10-06 11:06:06 +0200

    Update route.ts

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -1,42 +1,33 @@
-import prisma from '@/utils/db';
+import prisma from '@/app/utils/db';
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  try {
-    const { getUser } = getKindeServerSession();
-    const user = await getUser();
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
 
-    console.log('User from Kinde:', user); // Add this line
+  if (!user || user === null || !user.id) {
+    throw new Error('Something went wrong');
+  }
 
-    if (!user || !user.id) {
-      throw new Error('Something went wrong');
-    }
+  let dbUser = await prisma.user.findUnique({
+    where: {
+      kindeId: user.id,
+    },
+  });
 
-    let dbUser = await prisma.user.findUnique({
-      where: {
+  if (!dbUser) {
+    dbUser = await prisma.user.create({
+      data: {
         kindeId: user.id,
+        firstName: user.given_name ?? '',
+        lastName: user.family_name ?? '',
+        email: user.email ?? '',
+        profileImage:
+          user.picture ?? `https://avatar.vercel.sh/${user.given_name}`,
       },
     });
-
-    if (!dbUser) {
-      dbUser = await prisma.user.create({
-        data: {
-          kindeId: user.id,
-          firstName: user.given_name ?? '',
-          lastName: user.family_name ?? '',
-          email: user.email ?? '',
-          profileImage:
-            user.picture ?? `https://avatar.vercel.sh/${user.given_name}`,
-        },
-      });
-    }
-    return NextResponse.redirect('http://localhost:3000');
-  } catch (error) {
-    console.error('Error:', error);
-    return NextResponse.json(
-      { message: 'Failed to get user' },
-      { status: 500 }
-    );
   }
+
+  return NextResponse.redirect('http://localhost:3000/dashboard');
 }
