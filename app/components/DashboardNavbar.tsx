1003e691e86ef7bd3be5d68a89db367075565484
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-10-06 11:02:57 +0200

    Add files via upload

---
+++ app/components/DashboardNavbar.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Link from 'next/link';
+import { UsersRound } from 'lucide-react';
+
+const DashboardNavbar = () => {
+  return (
+    <div className="p-6 flex justify-between items-center">
+      <div className="flex item-center gap-3">
+        <div className="flex items-center">
+          <UsersRound />
+          <h2>HirCon</h2>
+        </div>
+        <Link href="/dashboard">Dashboard</Link>
+      </div>
+      <div className="flex gap-6">
+        <Link href="/profileEditing">Profile</Link>
+        <Link href="/company">Company</Link>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardNavbar;
