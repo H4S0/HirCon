f23c768aaa9aedaf8a164d50273fa8aa5a30f8fe
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-10-13 19:17:50 +0200

    Update actions.ts

--- app/actions.ts
+++ app/actions.ts
@@ -3,7 +3,7 @@
 import { parseWithZod } from '@conform-to/zod';
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import prisma from './utils/db';
-import { profileSchema } from './utils/zodSchemas';
+import { educationSchema, profileSchema } from './utils/zodSchemas';
 import { redirect } from 'next/navigation';
 
 export async function UpdateProfile(prevState: any, formData: FormData) {
@@ -22,14 +22,68 @@ export async function UpdateProfile(prevState: any, formData: FormData) {
     return submission.reply();
   }
 
-  // Store profile in database
-  const response = await prisma.profile.create({
-    data: {
-      description: submission.value.description,
-      skills: submission.value.skills,
-      education: submission.value.education,
-      experience: submission.value.experience,
+  const existingProfile = await prisma.profile.findUnique({
+    where: {
       userId: user.id,
+    },
+  });
+
+  if (existingProfile) {
+    await prisma.profile.update({
+      where: {
+        id: existingProfile.id,
+      },
+      data: {
+        description: submission.value.description,
+        skills: submission.value.skills,
+      },
+    });
+  } else {
+    await prisma.profile.create({
+      data: {
+        description: submission.value.description,
+        skills: submission.value.skills,
+        userId: user.id,
+      },
+    });
+  }
+
+  return redirect('/dashboard');
+}
+
+export async function CreateEducation(prevState: any, formData: FormData) {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user) {
+    return redirect('/api/auth/login');
+  }
+
+  const submission = parseWithZod(formData, {
+    schema: educationSchema,
+  });
+
+  if (submission.status !== 'success') {
+    return submission.reply();
+  }
+
+  const profile = await prisma.profile.findUnique({
+    where: {
+      userId: user.id,
+    },
+  });
+
+  if (!profile) {
+    return redirect('/profile/create');
+  }
+
+  const education = await prisma.education.create({
+    data: {
+      institution: submission.value.institution,
+      degree: submission.value.degree,
+      startDate: submission.value.startYear,
+      endDate: submission.value.endYear,
+      profileId: profile.id,
     },
   });
 
