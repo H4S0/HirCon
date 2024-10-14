5b08a862d31f791dd45ee905db3b23dcce5e4c7d
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-10-14 10:16:40 +0200

    Update zodSchemas.ts

--- app/utils/zodSchemas.ts
+++ app/utils/zodSchemas.ts
@@ -1,6 +1,5 @@
 import { z } from 'zod';
 
-// Degree enum for validation
 const DegreeEnum = z.enum([
   'HIGH_SCHOOL_DIPLOMA',
   'BACHELORS',
@@ -8,57 +7,21 @@ const DegreeEnum = z.enum([
   'DOCTORATE',
 ]);
 
-// Education schema
-export const educationSchema = z
-  .object({
-    institution: z.string().min(1, 'Institution is required'),
-    degree: DegreeEnum, // Use the enum for degree
-    startYear: z.number().int().min(1900).max(new Date().getFullYear()),
-    endYear: z
-      .number()
-      .int()
-      .min(1900)
-      .max(new Date().getFullYear())
-      .optional()
-      .refine(
-        (val) => {
-          return typeof val === 'undefined' || val >= 1900;
-        },
-        {
-          message: 'End year must be after the start year',
-        }
-      ),
-  })
-  .refine(
-    (data) => {
-      return !data.endYear || data.endYear >= data.startYear;
-    },
-    {
-      message: 'End year cannot be earlier than the start year',
-      path: ['endYear'], // This will target the specific field that fails validation
-    }
-  );
 
-// Experience schema
+export const educationSchema = z.object({
+  institution: z.string().min(1, 'Institution is required'),
+  degree: DegreeEnum, 
+  startYear: z.string(),
+  endYear: z.string(),
+});
+
+
 export const experienceSchema = z
   .object({
     company: z.string().min(1, 'Company is required'),
     role: z.string().min(1, 'Role is required'),
-    startYear: z.number().int().min(1900).max(new Date().getFullYear()),
-    endYear: z
-      .number()
-      .int()
-      .min(1900)
-      .max(new Date().getFullYear())
-      .optional()
-      .refine(
-        (val) => {
-          return typeof val === 'undefined' || val >= 1900;
-        },
-        {
-          message: 'End year must be after the start year',
-        }
-      ),
+    startYear: z.number(),
+    endYear: z.number(),
   })
   .refine(
     (data) => {
@@ -70,9 +33,8 @@ export const experienceSchema = z
     }
   );
 
-// Profile schema
 export const profileSchema = z.object({
-  description: z.string().optional(),
+  description: z.string(),
   currentStatus: z.enum(['opentowork', 'employed', 'unemployed']).optional(),
   skills: z.array(z.string()).optional(),
 });
