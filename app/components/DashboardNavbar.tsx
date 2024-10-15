b15a807d8a7e62d2bde2e6a8b7a5752691f8a8ab
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-10-15 18:52:12 +0200

    Update DashboardNavbar.tsx

--- app/components/DashboardNavbar.tsx
+++ app/components/DashboardNavbar.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import Link from 'next/link';
 import { UsersRound } from 'lucide-react';
+import {
+  getKindeServerSession,
+  LogoutLink,
+} from '@kinde-oss/kinde-auth-nextjs/server';
+import { Button } from '@/components/ui/button';
 
-const DashboardNavbar = () => {
+const DashboardNavbar = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = getUser();
+
   return (
     <div className="p-6 flex justify-between items-center">
       <div className="flex item-center gap-3">
@@ -12,9 +20,17 @@ const DashboardNavbar = () => {
         </div>
         <Link href="/dashboard">Dashboard</Link>
       </div>
-      <div className="flex gap-6">
+      <div className="flex gap-6 items-center">
+        <p>
+          Welcome{' '}
+          <span className="font-semibold">{(await user).given_name}</span>
+        </p>
+
         <Link href="/dashboard/profileediting">Profile</Link>
         <Link href="/dashboard/company">Company</Link>
+        <LogoutLink>
+          <Button>Logout</Button>
+        </LogoutLink>
       </div>
     </div>
   );
