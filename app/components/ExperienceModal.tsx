8764fbef9a86488d289da16570c8771d75310eed
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-10-18 14:59:57 +0200

    Update ExperienceModal.tsx

--- app/components/ExperienceModal.tsx
+++ app/components/ExperienceModal.tsx
@@ -13,11 +13,33 @@ import {
 } from '@/components/ui/alert-dialog';
 import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
-import React from 'react';
+import React, { useActionState } from 'react';
 import { DatePicker } from './DatePicker';
 import { Button } from '@/components/ui/button';
+import { CreateExperience } from '../actions';
+import { useForm } from '@conform-to/react';
+import { parseWithZod } from '@conform-to/zod';
+import { experienceSchema } from '../utils/zodSchemas';
 
 const ExperienceModal = () => {
+  const [lastResult, action] = useActionState(CreateExperience, undefined);
+  const [form, fields] = useForm({
+    lastResult,
+    onValidate({ formData }) {
+      return parseWithZod(formData, {
+        schema: experienceSchema,
+      });
+    },
+    shouldValidate: 'onBlur',
+    shouldRevalidate: 'onInput',
+  });
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+
+    const result = await CreateExperience(undefined, formData);
+  };
   return (
     <>
       <AlertDialog>
@@ -25,37 +47,41 @@ const ExperienceModal = () => {
           <Button>Add Experience</Button>
         </AlertDialogTrigger>
         <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-            <AlertDialogDescription>
-              <div className="grid gap-6 mt-5">
-                <div className="grid gap-2">
-                  <Label>Company name</Label>
-                  <Input />
+          <form id={form.id} onSubmit={handleSubmit} action={action}>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+              <AlertDialogDescription>
+                <div className="grid gap-6 mt-5">
+                  <div className="grid gap-2">
+                    <Label>Company name</Label>
+                    <Input placeholder="Add Company name" />
+                  </div>
+                  <div className="grid gap-2">
+                    <Label>Add your role</Label>
+                    <Input placeholder="Add your role" />
+                  </div>
+                  <div className="grid gap-3">
+                    <Label>Starting date</Label>
+                    <DatePicker />
+                  </div>
+                  <div className="grid gap-3">
+                    <Label>End date</Label>
+                    <DatePicker />
+                  </div>
+                  <div className="grid gap-3">
+                    <Label>Short description abour your role</Label>
+                    <Input placeholder="Describe your role" />
+                  </div>
                 </div>
-                <div className="grid gap-2">
-                  <Label>Add your role</Label>
-                  <Input placeholder="Add your role" />
-                </div>
-                <div className="grid gap-3">
-                  <Label>Starting date</Label>
-                  <DatePicker />
-                </div>
-                <div className="grid gap-3">
-                  <Label>End date</Label>
-                  <DatePicker />
-                </div>
-                <div>
-                  <Label>Short description abour your role</Label>
-                  <Input placeholder="Describe your role" />
-                </div>
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <div className="grid grid-cols-2 gap-2 mt-5">
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction>Continue</AlertDialogAction>
               </div>
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel>Cancel</AlertDialogCancel>
-            <AlertDialogAction>Continue</AlertDialogAction>
-          </AlertDialogFooter>
+            </AlertDialogFooter>
+          </form>
         </AlertDialogContent>
       </AlertDialog>
     </>
