e8fcfc771f0ddb3947771bbf5a5b714062276f10
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-10-19 12:53:09 +0200

    Update ExperienceModal.tsx

--- app/components/ExperienceModal.tsx
+++ app/components/ExperienceModal.tsx
@@ -9,12 +9,10 @@ import {
   AlertDialogTitle,
   AlertDialogDescription,
   AlertDialogCancel,
-  AlertDialogAction,
 } from '@/components/ui/alert-dialog';
 import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
 import React, { useActionState } from 'react';
-import { DatePicker } from './DatePicker';
 import { Button } from '@/components/ui/button';
 import { CreateExperience } from '../actions';
 import { useForm } from '@conform-to/react';
@@ -47,39 +45,67 @@ const ExperienceModal = () => {
           <Button>Add Experience</Button>
         </AlertDialogTrigger>
         <AlertDialogContent>
-          <form id={form.id} onSubmit={handleSubmit} action={action}>
+          <form
+            id={form.id}
+            onSubmit={handleSubmit}
+            action={action}
+            className="grid gap-5"
+          >
             <AlertDialogHeader>
               <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
               <AlertDialogDescription>
                 <div className="grid gap-6 mt-5">
                   <div className="grid gap-2">
                     <Label>Company name</Label>
-                    <Input placeholder="Add Company name" />
+                    <Input
+                      key={fields.company.key}
+                      name={fields.company.name}
+                      defaultValue={fields.company.initialValue}
+                      placeholder="Add Company name"
+                    />
                   </div>
                   <div className="grid gap-2">
                     <Label>Add your role</Label>
-                    <Input placeholder="Add your role" />
+                    <Input
+                      key={fields.role.key}
+                      name={fields.role.name}
+                      defaultValue={fields.role.initialValue}
+                      placeholder="Add your role"
+                    />
                   </div>
                   <div className="grid gap-3">
                     <Label>Starting date</Label>
-                    <DatePicker />
+                    <Input
+                      type="date"
+                      name={fields.startYear.name}
+                      key={fields.startYear.key}
+                      defaultValue={fields.startYear.initialValue}
+                    />
                   </div>
                   <div className="grid gap-3">
                     <Label>End date</Label>
-                    <DatePicker />
+                    <Input
+                      type="date"
+                      name={fields.endYear.name}
+                      key={fields.endYear.key}
+                      defaultValue={fields.endYear.initialValue}
+                    />
                   </div>
                   <div className="grid gap-3">
                     <Label>Short description abour your role</Label>
-                    <Input placeholder="Describe your role" />
+                    <Input
+                      placeholder="Describe your role"
+                      key={fields.roleDescription.key}
+                      name={fields.roleDescription.name}
+                      defaultValue={fields.roleDescription.initialValue}
+                    />
                   </div>
                 </div>
               </AlertDialogDescription>
             </AlertDialogHeader>
             <AlertDialogFooter>
-              <div className="grid grid-cols-2 gap-2 mt-5">
-                <AlertDialogCancel>Cancel</AlertDialogCancel>
-                <AlertDialogAction>Continue</AlertDialogAction>
-              </div>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <Button>Submit</Button>
             </AlertDialogFooter>
           </form>
         </AlertDialogContent>
