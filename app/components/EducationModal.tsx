a997bee09ce8b75325fc43b396e54672e3b401ae
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-10-20 13:11:27 +0200

    Update EducationModal.tsx

--- app/components/EducationModal.tsx
+++ app/components/EducationModal.tsx
@@ -23,21 +23,6 @@ import { educationSchema } from '@/app/utils/zodSchemas';
 import { Button } from '@/components/ui/button';
 import { parseWithZod } from '@conform-to/zod';
 import { CreateEducation } from '../actions';
-import prisma from '../utils/db';
-import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
-import { redirect } from 'next/navigation';
-
-async function getData(userId: string) {
-  const data = await prisma.profile.findMany({
-    where: {
-      userId: userId,
-    },
-    orderBy: {
-      createdAt: 'desc',
-    },
-  });
-  return data;
-}
 
 interface Data {
   id: string;
@@ -71,21 +56,19 @@ const EducationModal = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      const { getUser } = getKindeServerSession();
-      const user = await getUser();
-
-      if (!user) {
-        return redirect('/api/auth/login');
-      } else {
-        const profileData = await getData(user.id);
-        setData(profileData);
-      }
+      const response = await fetch('/api/gettingProfile');
+      const result = await response.json();
+      setData(result);
     };
     fetchData();
   }, []);
 
   const checkingData = data?.map((item) => {
-    return item.description, item.skills;
+    return item.description;
+  });
+
+  const skillsData = data?.map((item) => {
+    return item.skills;
   });
 
   return (
@@ -95,81 +78,89 @@ const EducationModal = () => {
           <Button>Add Education</Button>
         </AlertDialogTrigger>
         <AlertDialogContent>
-          <form
-            id={form.id}
-            onSubmit={handleSubmit}
-            action={action}
-            className="grid gap-5"
-          >
-            <AlertDialogHeader>
-              <AlertDialogTitle>
-                Are you sure you want to add this education?
-              </AlertDialogTitle>
-              <AlertDialogDescription>
-                <div className="grid gap-6 mt-5">
-                  <div className="grid gap-2">
-                    <Label htmlFor={fields.institution.name}>
-                      Institution name
-                    </Label>
-                    <Input
-                      key={fields.institution.key}
-                      name={fields.institution.name}
-                      defaultValue={fields.institution.initialValue}
-                      placeholder="Add Institution name"
-                    />
-                  </div>
+          {checkingData == 0 ? (
+            'Please make sure to fill descrtiption first'
+          ) : (
+            <form
+              id={form.id}
+              onSubmit={handleSubmit}
+              action={action}
+              className="grid gap-5"
+            >
+              <AlertDialogHeader>
+                <AlertDialogTitle>
+                  Are you sure you want to add this education?
+                </AlertDialogTitle>
+                <AlertDialogDescription>
+                  <div className="grid gap-6 mt-5">
+                    <div className="grid gap-2">
+                      <Label htmlFor={fields.institution.name}>
+                        Institution name
+                      </Label>
+                      <Input
+                        key={fields.institution.key}
+                        name={fields.institution.name}
+                        defaultValue={fields.institution.initialValue}
+                        placeholder="Add Institution name"
+                      />
+                    </div>
 
-                  <div className="grid gap-2">
-                    <Label>Degree</Label>
-                    <Select
-                      name={fields.degree.name}
-                      defaultValue={fields.degree.initialValue}
-                    >
-                      <SelectTrigger className="w-[180px]">
-                        <SelectValue placeholder="Choose your degree" />
-                      </SelectTrigger>
-                      <SelectContent>
-                        <SelectItem value="HIGH_SCHOOL_DIPLOMA">
-                          High School Diploma
-                        </SelectItem>
-                        <SelectItem value="BACHELORS">
-                          Bachelor's Degree
-                        </SelectItem>
-                        <SelectItem value="MASTERS">Master's Degree</SelectItem>
-                        <SelectItem value="DOCTORATE">
-                          Doctorate Degree
-                        </SelectItem>
-                      </SelectContent>
-                    </Select>
-                  </div>
+                    <div className="grid gap-2">
+                      <Label>Degree</Label>
+                      <Select
+                        name={fields.degree.name}
+                        defaultValue={fields.degree.initialValue}
+                      >
+                        <SelectTrigger className="w-[180px]">
+                          <SelectValue placeholder="Choose your degree" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          <SelectItem value="HIGH_SCHOOL_DIPLOMA">
+                            High School Diploma
+                          </SelectItem>
+                          <SelectItem value="BACHELORS">
+                            Bachelor's Degree
+                          </SelectItem>
+                          <SelectItem value="MASTERS">
+                            Master's Degree
+                          </SelectItem>
+                          <SelectItem value="DOCTORATE">
+                            Doctorate Degree
+                          </SelectItem>
+                        </SelectContent>
+                      </Select>
+                    </div>
 
-                  <div className="grid gap-3">
-                    <Label htmlFor={fields.startYear.name}>Starting date</Label>
-                    <Input
-                      key={fields.startYear.key}
-                      name={fields.startYear.name}
-                      defaultValue={fields.startYear.initialValue}
-                      placeholder="Insert start date"
-                    />
-                  </div>
+                    <div className="grid gap-3">
+                      <Label htmlFor={fields.startYear.name}>
+                        Starting date
+                      </Label>
+                      <Input
+                        key={fields.startYear.key}
+                        name={fields.startYear.name}
+                        defaultValue={fields.startYear.initialValue}
+                        placeholder="Insert start date"
+                      />
+                    </div>
 
-                  <div className="grid gap-3">
-                    <Label htmlFor={fields.endYear.name}>End date</Label>
-                    <Input
-                      key={fields.endYear.key}
-                      name={fields.endYear.name}
-                      defaultValue={fields.endYear.initialValue}
-                      placeholder="Insert end date"
-                    />
+                    <div className="grid gap-3">
+                      <Label htmlFor={fields.endYear.name}>End date</Label>
+                      <Input
+                        key={fields.endYear.key}
+                        name={fields.endYear.name}
+                        defaultValue={fields.endYear.initialValue}
+                        placeholder="Insert end date"
+                      />
+                    </div>
                   </div>
-                </div>
-              </AlertDialogDescription>
-            </AlertDialogHeader>
-            <AlertDialogFooter>
-              <AlertDialogCancel>Cancel</AlertDialogCancel>
-              <Button type="submit">Submit</Button>
-            </AlertDialogFooter>
-          </form>
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <Button type="submit">Submit</Button>
+              </AlertDialogFooter>
+            </form>
+          )}
         </AlertDialogContent>
       </AlertDialog>
     </div>
