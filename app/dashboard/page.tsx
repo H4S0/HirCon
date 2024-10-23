3e7976108e359ea0cad52fd796ec4b2d48d0bd45
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-10-23 17:40:45 +0200

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,17 +3,38 @@ import DashboardNavbar from '../components/DashboardNavbar';
 
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { redirect } from 'next/navigation';
+import prisma from '../utils/db';
+
+async function getData(userId: string) {
+  const data = await prisma.profile.findMany({
+    where: {
+      userId: userId,
+    },
+  });
+  return data;
+}
 
 const page = async () => {
-  const { isAuthenticated } = getKindeServerSession();
+  const { isAuthenticated, getUser } = getKindeServerSession();
   const isUserAuthenticated = await isAuthenticated();
+  const user = await getUser();
 
   if (!isUserAuthenticated) {
     return redirect('/api/auth/login');
   }
+
+  const data = await getData(user.id);
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mb-24">
       <DashboardNavbar />
+      <div>
+        <h2>Data</h2>
+        {data.map((item) => (
+          <>
+            <h2>{item.description}</h2>
+          </>
+        ))}
+      </div>
     </div>
   );
 };
