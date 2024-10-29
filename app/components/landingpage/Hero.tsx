2fde88e9c631013b8d952bb567ec20f4e1e02202
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-10-29 18:52:59 +0100

    Update Hero.tsx

--- app/components/landingpage/Hero.tsx
+++ app/components/landingpage/Hero.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
 import Navbar from '../LandingPageNavbar';
+import { Input } from '@/components/ui/input';
 const Hero = () => {
   return (
     <>
       <Navbar />
+      <div>
+        <div>
+          <div>
+            <p>Find Your</p>
+            <p>Ideal Job</p>
+          </div>
+          <Input placeholder="Search job" />
+          <div>
+            <div>
+              <p>8M+</p>
+              <p>Matched Made</p>
+            </div>
+            <div>
+              <p>Unlocking your potential</p>
+              <p>View all job offers</p>
+            </div>
+          </div>
+        </div>
+        <h3>SLIKAA</h3>
+      </div>
     </>
   );
 };
