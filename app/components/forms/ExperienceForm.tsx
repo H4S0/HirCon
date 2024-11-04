58f20a9ecf87b7bee6cf763be6f05b4a92a51941
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-11-04 13:41:57 +0100

    Update ExperienceForm.tsx

--- app/components/forms/ExperienceForm.tsx
+++ app/components/forms/ExperienceForm.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import React, { useActionState, useState } from 'react';
 import { updateExperience } from '@/app/actions';
 import { experienceSchema } from '@/app/utils/zodSchemas';
 import { Button } from '@/components/ui/button';
@@ -14,7 +15,6 @@ import { Label } from '@/components/ui/label';
 import { Textarea } from '@/components/ui/textarea';
 import { useForm } from '@conform-to/react';
 import { parseWithZod } from '@conform-to/zod';
-import React, { useActionState, useState } from 'react';
 
 interface ExperienceDataProps {
   company: string;
@@ -31,117 +31,102 @@ const ExperienceForm = ({
   data: ExperienceDataProps;
   experienceId: string;
 }) => {
-  const [company, setCompany] = useState<string | undefined>(data.company);
-  const [role, setRole] = useState<string | undefined>(data.role);
-  const [roleDescription, setRoleDescripiton] = useState<string | undefined>(
-    data.roleDescription
-  );
-  const [startDate, setStartDate] = useState<string | undefined>(
-    data.startDate
-  );
-  const [endDate, setEndDate] = useState<string | undefined>(data.endDate);
+  const [company, setCompany] = useState(data.company);
+  const [role, setRole] = useState(data.role);
+  const [roleDescription, setRoleDescription] = useState(data.roleDescription);
+  const [startDate, setStartDate] = useState(data.startDate);
+  const [endDate, setEndDate] = useState(data.endDate);
+
   const [lastResult, action] = useActionState(updateExperience, undefined);
   const [form, fields] = useForm({
     lastResult,
     onValidate({ formData }) {
-      return parseWithZod(formData, {
-        schema: experienceSchema,
-      });
+      return parseWithZod(formData, { schema: experienceSchema });
     },
     shouldValidate: 'onBlur',
     shouldRevalidate: 'onInput',
   });
 
   return (
-    <>
-      <Card className="max-w-md w-full shadow-lg">
-        <CardHeader>
-          <h1 className="text-lg font-bold">Edit Experience</h1>
-        </CardHeader>
-        <CardContent>
-          {data ? (
-            <form id={form.id} onSubmit={form.onSubmit} action={action}>
-              <input type="hidden" name="experienceId" value={experienceId} />
+    <Card className="max-w-md w-full shadow-lg">
+      <CardHeader>
+        <h1 className="text-lg font-bold">Edit Experience</h1>
+      </CardHeader>
+      <CardContent>
+        <form id={form.id} onSubmit={form.onSubmit} action={action}>
+          <input type="hidden" name="experienceId" value={experienceId} />
 
-              {/* Company/Institution */}
-              <div className="mb-4">
-                <Label htmlFor="company">Company/Institution:</Label>
-                <Input
-                  id={fields.company.name}
-                  name={fields.company.name}
-                  defaultValue={fields.company.initialValue}
-                  required
-                  className="mt-1"
-                  value={company}
-                />
-              </div>
+          {/* Company/Institution */}
+          <div className="mb-4">
+            <Label htmlFor="company">Company/Institution:</Label>
+            <Input
+              id={fields.company.name}
+              name={fields.company.name}
+              defaultValue={company}
+              required
+              onChange={(e) => setCompany(e.target.value)}
+            />
+          </div>
 
-              {/* Role */}
-              <div className="mb-4">
-                <Label htmlFor="role">Role:</Label>
-                <Input
-                  id={fields.role.name}
-                  name={fields.role.name}
-                  defaultValue={fields.role.initialValue}
-                  required
-                  className="mt-1"
-                  value={role}
-                />
-              </div>
+          {/* Role */}
+          <div className="mb-4">
+            <Label htmlFor="role">Role:</Label>
+            <Input
+              id={fields.role.name}
+              name={fields.role.name}
+              defaultValue={role}
+              required
+              onChange={(e) => setRole(e.target.value)}
+            />
+          </div>
 
-              {/* Role Description */}
-              <div className="mb-4">
-                <Label htmlFor="roleDescription">Role Description:</Label>
-                <Textarea
-                  id={fields.roleDescription.name}
-                  name={fields.roleDescription.name}
-                  defaultValue={fields.roleDescription.initialValue}
-                  required
-                  className="mt-1"
-                  rows={4}
-                  value={roleDescription}
-                />
-              </div>
+          {/* Role Description */}
+          <div className="mb-4">
+            <Label htmlFor="roleDescription">Role Description:</Label>
+            <Textarea
+              id={fields.roleDescription.name}
+              name={fields.roleDescription.name}
+              defaultValue={roleDescription}
+              required
+              onChange={(e) => setRoleDescription(e.target.value)}
+              rows={4}
+            />
+          </div>
 
-              {/* Start Date */}
-              <div className="mb-4">
-                <Label htmlFor="startDate">Start Date:</Label>
-                <Input
-                  id={fields.startYear.name}
-                  name={fields.startYear.name}
-                  type="date"
-                  defaultValue={fields.startYear.initialValue}
-                  required
-                  className="mt-1"
-                  value={startDate}
-                />
-              </div>
+          {/* Start Date */}
+          <div className="mb-4">
+            <Label htmlFor="startDate">Start Date:</Label>
+            <Input
+              id={fields.startYear.name}
+              name={fields.startYear.name}
+              type="date"
+              defaultValue={startDate}
+              required
+              onChange={(e) => setStartDate(e.target.value)}
+            />
+          </div>
 
-              {/* End Date */}
-              <div className="mb-4">
-                <Label htmlFor="endDate">End Date:</Label>
-                <Input
-                  id={fields.endYear.name}
-                  name={fields.endYear.name}
-                  type="date"
-                  defaultValue={fields.endYear.initialValue}
-                  required
-                  className="mt-1"
-                  value={endDate}
-                />
-              </div>
-            </form>
-          ) : (
-            <p>No experience details found.</p>
-          )}
-        </CardContent>
-        <CardFooter>
-          <Button type="submit" form="edit-experience-form" className="w-full">
-            Save
-          </Button>
-        </CardFooter>
-      </Card>
-    </>
+          {/* End Date */}
+          <div className="mb-4">
+            <Label htmlFor="endDate">End Date:</Label>
+            <Input
+              id={fields.endYear.name}
+              name={fields.endYear.name}
+              type="date"
+              defaultValue={endDate}
+              required
+              onChange={(e) => setEndDate(e.target.value)}
+            />
+          </div>
+
+          <CardFooter>
+            <Button type="submit" className="w-full">
+              Save
+            </Button>
+          </CardFooter>
+        </form>
+      </CardContent>
+    </Card>
   );
 };
 
