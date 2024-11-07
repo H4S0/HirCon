d03991de937dea774556e994c6f753359cc17d9a
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-11-07 20:50:04 +0100

    Update DashboardNavbar.tsx

--- app/components/DashboardNavbar.tsx
+++ app/components/DashboardNavbar.tsx
@@ -2,13 +2,14 @@
 
 import React, { useState } from 'react';
 import Link from 'next/link';
-import { UsersRound, Menu, X } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { LogoutLink } from '@kinde-oss/kinde-auth-nextjs/components';
+import { UsersRound } from 'lucide-react';
+import { Menu, X } from 'lucide-react';
 import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs';
 
 const DashboardNavbar = () => {
-  const { user, isLoading } = useKindeBrowserClient();
+  const { user, isAuthenticated, isLoading } = useKindeBrowserClient();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const toggleMenu = () => {
@@ -16,69 +17,76 @@ const DashboardNavbar = () => {
   };
 
   return (
-    <div className="p-6 flex justify-between items-center border-b border-gray-300">
-      <div className="flex items-center gap-6">
+    <>
+      <div className="flex justify-between py-6 items-center px-4 md:px-8 border-b border-gray-300">
         <Link href={'/'}>
           <div className="flex items-center">
             <UsersRound className="mr-2" />
             <h2 className="text-lg font-semibold">HirCon</h2>
           </div>
         </Link>
-        <div className="hidden md:flex gap-6 items-center">
-          <Link href="/dashboard" className="hover:underline">
+        <div className="hidden md:flex gap-6">
+          <Link href={'/dashboard'} className="hover:underline">
             Dashboard
           </Link>
-          <Link href="/dashboard/profileediting" className="hover:underline">
+          <Link href={'/dashboard/profileediting'} className="hover:underline">
             Profile
           </Link>
-          <Link href="/dashboard/company" className="hover:underline">
+          <Link href={'/dashboard/company'} className="hover:underline">
             Company
           </Link>
         </div>
+        <div className="hidden md:flex gap-5">
+          {isLoading ? (
+            <p className="font-bold">loading...</p>
+          ) : isAuthenticated ? (
+            <div className="flex items-center gap-2">
+              <p className="font-semibold">{user?.given_name}</p>
+
+              <LogoutLink>
+                <Button>Logout</Button>
+              </LogoutLink>
+            </div>
+          ) : (
+            <>
+              <Link href="/register">
+                <Button>Register</Button>
+              </Link>
+              <Link href="/login">
+                <Button>Login</Button>
+              </Link>
+            </>
+          )}
+        </div>
+        <div className="md:hidden">
+          <button onClick={toggleMenu}>
+            {isMenuOpen ? <X size={24} /> : <Menu size={24} />}
+          </button>
+        </div>
       </div>
 
-      <div className="hidden md:flex gap-6 items-center">
-        <p>
-          Welcome{' '}
-          <span className="font-semibold">
-            {isLoading ? 'loading...' : user?.given_name}
-          </span>
-        </p>
-        <LogoutLink>
-          <Button>Logout</Button>
-        </LogoutLink>
-      </div>
-
-      {/* Mobile Menu Toggle */}
-      <div className="md:hidden">
-        <button onClick={toggleMenu}>
-          {isMenuOpen ? <X size={24} /> : <Menu size={24} />}
-        </button>
-      </div>
-
-      {/* Mobile Menu */}
       <div
         className={`${
           isMenuOpen ? 'max-h-screen opacity-100' : 'max-h-0 opacity-0'
-        } transition-all duration-300 ease-in-out overflow-hidden md:hidden bg-white w-full border-t border-gray-300`}
+        } transition-all duration-300 ease-in-out overflow-hidden md:hidden bg-white border-b border-gray-300`}
       >
         <div className="flex flex-col items-center gap-4 py-4">
           <Link
-            href="/dashboard"
+            href={'/dashboard'}
             onClick={toggleMenu}
             className="hover:underline"
           >
             Dashboard
           </Link>
           <Link
-            href="/dashboard/profileediting"
+            href={'/dashboard/profileediting'}
             onClick={toggleMenu}
             className="hover:underline"
           >
             Profile
           </Link>
           <Link
-            href="/dashboard/company"
+            href={'/dashboard/company'}
             onClick={toggleMenu}
             className="hover:underline"
           >
@@ -87,12 +95,23 @@ const DashboardNavbar = () => {
           <p className="font-semibold">
             Welcome {isLoading ? 'loading...' : user?.given_name}
           </p>
-          <LogoutLink>
-            <Button onClick={toggleMenu}>Logout</Button>
-          </LogoutLink>
+          {isAuthenticated ? (
+            <LogoutLink>
+              <Button onClick={toggleMenu}>Logout</Button>
+            </LogoutLink>
+          ) : (
+            <>
+              <Link href="/register">
+                <Button onClick={toggleMenu}>Register</Button>
+              </Link>
+              <Link href="/login">
+                <Button onClick={toggleMenu}>Login</Button>
+              </Link>
+            </>
+          )}
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
