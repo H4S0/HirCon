79a2dcd9d8238ee04cb11e5b07e7fdfa1503cc03
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-11-07 20:32:05 +0100

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -88,7 +88,6 @@ const ProfilePage = async () => {
         >
           {userData ? (
             <div className="flex flex-col items-center gap-6">
-              {/* Profile Header */}
               <div className="flex items-center gap-4 mb-6">
                 <Image
                   src={userData.profileImage}
@@ -113,15 +112,17 @@ const ProfilePage = async () => {
                 </div>
               </div>
 
-              {/* About Section */}
               <div className="w-full bg-gray-100 p-6 rounded-lg shadow-inner mb-6">
                 <h3 className="text-lg font-medium text-gray-700 mb-2">
                   About
                 </h3>
                 <p className="text-gray-600">{item.description}</p>
+                <p>
+                  <span className="font-semibold mr-1">Contact :</span>
+                  {item.contact}
+                </p>
               </div>
 
-              {/* Skills Section */}
               <div className="w-full bg-gray-100 p-6 rounded-lg shadow-inner mb-6">
                 <h3 className="text-lg font-medium text-gray-700 mb-2">
                   Skills
@@ -142,7 +143,6 @@ const ProfilePage = async () => {
                 )}
               </div>
 
-              {/* Experience Section */}
               <div className="w-full bg-gray-100 p-6 rounded-lg shadow-inner mb-6">
                 <div className="flex items-center justify-between">
                   <h3 className="text-lg font-medium text-gray-700 mb-2">
@@ -175,8 +175,6 @@ const ProfilePage = async () => {
                   <p>No experience</p>
                 )}
               </div>
-
-              {/* Education Section */}
               <div className="w-full bg-gray-100 p-6 rounded-lg shadow-inner mb-6">
                 <div className="flex items-center justify-between">
                   <h3 className="text-lg font-medium text-gray-700 mb-2">
