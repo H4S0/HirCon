63ef37bfc9b04c8bfafe1a31891916e6e69081d9
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-11-10 15:35:21 +0100

    Update page.tsx

--- app/dashboard/company/page.tsx
+++ app/dashboard/company/page.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+'use client';
+
+import React, { useActionState } from 'react';
+import Link from 'next/link';
+import DashboardNavbar from '@/app/components/DashboardNavbar';
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mb-24">
+      <DashboardNavbar />
+      <Link href={'/dashboard/company/new'}>New Company</Link>
+    </div>
+  );
 };
 
 export default page;
