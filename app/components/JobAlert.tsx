c0586d193566b870ca5e397ecb6ca3b7c76d5afe
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-11-27 21:11:56 +0100

    Update JobAlert.tsx

--- app/components/JobAlert.tsx
+++ app/components/JobAlert.tsx
@@ -3,25 +3,38 @@ import { JobAlertProps } from '../dashboard/company/page';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 
-const JobAlert = ({ data }: { data: JobAlertProps[] }) => {
+const JobAlert = ({
+  data,
+  currentRoute,
+}: {
+  data: JobAlertProps[];
+  currentRoute: string;
+}) => {
   return (
     <div className="p-4 min-h-screen">
-      <h1 className="text-2xl font-bold mb-6 text-center">Job Alerts</h1>
       <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
         {data.map((item) => (
-          <JobCard key={item.id} item={item} />
+          <JobCard key={item.id} item={item} currentRoute={currentRoute} />
         ))}
       </div>
     </div>
   );
 };
 
-const JobCard = ({ item }: { item: JobAlertProps }) => {
+const JobCard = ({
+  item,
+  currentRoute,
+}: {
+  item: JobAlertProps;
+  currentRoute: string;
+}) => {
   const [expanded, setExpanded] = useState(false);
+
+  const isCompanyDashboard = currentRoute === '/dashboard/company';
 
   return (
     <div className="border rounded-lg shadow-md bg-white p-4 hover:shadow-lg transition">
-      <h2 className="text-xl font-semibold ">{item.jobTitle}</h2>
+      <h2 className="text-xl font-semibold">{item.jobTitle}</h2>
       <p className="text-gray-500 text-sm mb-2">
         <span className="font-medium">Location:</span> {item.location}{' '}
         {item.remote === 'true' && (
@@ -38,8 +51,7 @@ const JobCard = ({ item }: { item: JobAlertProps }) => {
         <span className="font-medium">Salary:</span> ${item.salary}
       </p>
       <p className="text-gray-500 text-sm mb-2">
-        <span className="font-medium">Remote:</span>
-        {item.remote}
+        <span className="font-medium">Remote:</span> {item.remote}
       </p>
       <p className={`text-gray-600 mb-4 ${expanded ? '' : 'line-clamp-3'}`}>
         {item.jobDescription}
@@ -50,9 +62,15 @@ const JobCard = ({ item }: { item: JobAlertProps }) => {
       >
         {expanded ? 'Show Less' : 'Read More'}
       </button>
-      <Link href={`/dashboard/company/job/${item.id}`}>
-        <Button className="w-full">Edit your job alert</Button>
-      </Link>
+      {isCompanyDashboard ? (
+        <Link href={`/dashboard/company/job/${item.id}`}>
+          <Button className="w-full">Edit your job alert</Button>
+        </Link>
+      ) : (
+        <Link href={`/apply/job/${item.id}`}>
+          <Button className="w-full">Apply to Job</Button>
+        </Link>
+      )}
     </div>
   );
 };
