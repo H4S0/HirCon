51a30c0af3dc74292e84bfcedd604859fac119c4
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-11-27 21:12:09 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Hero from './components/landingpage/Hero';
 import Process from './components/landingpage/Process';
+import JobList from './components/landingpage/JobList';
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mb-24">
         <Hero />
         <Process />
+        <JobList />
       </div>
     </>
   );
