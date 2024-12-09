b03220b9862ef90213fc553469b28bc39e85a22d
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-12-09 17:46:07 +0100

    Update route.ts

--- app/api/gettingJobAlert/route.ts
+++ app/api/gettingJobAlert/route.ts
@@ -1,19 +1,8 @@
 import prisma from '@/app/utils/db';
-import { requireUser } from '@/app/utils/requireUser';
-import { redirect } from 'next/navigation';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  const user = requireUser();
-
-  if (!user) {
-    return redirect('/api/auth/login');
-  }
-
   const data = await prisma.jobAlert.findMany({
-    where: {
-      companyId: (await user).id,
-    },
     select: {
       jobTitle: true,
       jobDescription: true,
