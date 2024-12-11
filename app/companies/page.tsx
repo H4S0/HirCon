3859f0592b2461759196c43e9bb6c9782575d9ac
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-12-11 12:25:26 +0100

    Update page.tsx

--- app/companies/page.tsx
+++ app/companies/page.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Navbar from '../components/LandingPageNavbar';
 import prisma from '../utils/db';
+import CompanyCard from '../components/CompanyCard';
+import { Input } from '@/components/ui/input';
 
 const Page = async () => {
   const companyData = await prisma.company.findMany({
@@ -19,13 +21,12 @@ const Page = async () => {
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mb-24">
       <Navbar />
       <h1 className="text-2xl font-bold mb-4">Companies</h1>
-      <ul>
+      <Input />
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {companyData.map((company, index) => (
-          <li key={index} className="mb-2">
-            {company.companyName}
-          </li>
+          <CompanyCard company={company} key={index} />
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
