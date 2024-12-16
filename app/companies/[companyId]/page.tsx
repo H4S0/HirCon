d0e7cce8840f2cb9067017ae94fd01db71ea40b3
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-12-16 11:49:27 +0100

    Update page.tsx

--- app/companies/[companyId]/page.tsx
+++ app/companies/[companyId]/page.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { getCompanyId } from '@/app/dashboard/company/companyEdit/[companyId]/page';
+import Navbar from '@/app/components/LandingPageNavbar';
 
-const page = () => {
-  return <div>page</div>;
+interface Params {
+  companyId: string;
+}
+
+const page = async ({ params }: { params: Params }) => {
+  const { companyId } = params;
+  const companyData = await getCompanyId(companyId);
+
+  return (
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mb-24">
+      <Navbar />
+    </div>
+  );
 };
 
 export default page;
