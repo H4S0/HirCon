6a488be4da32546c2194ecf8864a2b506bf66edc
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-12-16 11:28:53 +0100

    Update Footer.tsx

--- app/components/landingpage/Footer.tsx
+++ app/components/landingpage/Footer.tsx
@@ -1,13 +1,14 @@
+'use client';
+
 import { Facebook, Instagram, Linkedin, Twitter } from 'lucide-react';
 import React from 'react';
+import GetStarted from '../GetStarted';
 
 const Footer = () => {
   return (
     <footer className="text-black">
       <div className="container mx-auto px-6">
-        {/* Top Section */}
         <div className="flex flex-wrap justify-between gap-y-8">
-          {/* Company Info */}
           <div className="w-full md:w-1/2 lg:w-1/3">
             <h3 className="text-2xl font-bold mb-4">Hircon</h3>
             <p className="text-gray-600 text-lg md:text-base">
@@ -16,12 +17,9 @@ const Footer = () => {
               career-defining opportunities for job seekers, while empowering
               companies to find the right talent with ease.
             </p>
-            <button className="mt-6 bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-6 rounded-lg transition-all duration-300">
-              Register Now
-            </button>
+            <GetStarted />
           </div>
 
-          {/* Quick Links */}
           <div className="w-full md:w-1/2 lg:w-1/3">
             <h4 className="text-xl font-semibold mb-4">Quick Links</h4>
             <ul className="space-y-2">
@@ -48,7 +46,6 @@ const Footer = () => {
             </ul>
           </div>
 
-          {/* Contact Info */}
           <div className="w-full md:w-1/2 lg:w-1/3">
             <h4 className="text-xl font-semibold mb-4">Contact Us</h4>
             <p className="text-gray-600">📍 Tešanj, Bosnia and Herzegovina</p>
@@ -88,7 +85,6 @@ const Footer = () => {
           </div>
         </div>
 
-        {/* Bottom Section */}
         <div className="mt-12 border-t border-gray-700 pt-6 text-center">
           <p className="text-gray-500">
             © {new Date().getFullYear()} Hircon. All rights reserved.
