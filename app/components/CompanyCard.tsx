065f4a4e15db95d6a703d42c675c4b1ccc2d64ae
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-12-20 15:49:12 +0100

    Update CompanyCard.tsx

--- app/components/CompanyCard.tsx
+++ app/components/CompanyCard.tsx
@@ -1,13 +1,29 @@
 import { Button } from '@/components/ui/button';
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
 const CompanyCard = ({ company }) => {
   return (
     <div className="bg-white shadow-lg rounded-lg p-6 hover:shadow-xl transition-shadow border border-gray-100 flex flex-col">
-      <h2 className="text-2xl font-semibold mb-4 text-gray-900">
-        {company.companyName}
-      </h2>
+      <div className="flex gap-3 items-center justify-start pb-5">
+        {company.image ? (
+          <Image
+            src={company.image}
+            width={66}
+            height={66}
+            alt="company-logo"
+            className="object-cover rounded-full"
+          />
+        ) : (
+          <span className="text-gray-500 text-sm font-semibold">
+            Company Logo
+          </span>
+        )}
+        <h2 className="text-2xl font-semibold text-gray-900">
+          {company.companyName}
+        </h2>
+      </div>
 
       <p className="text-sm text-gray-700 mb-2">
         <strong className="font-medium text-gray-800">Industry:</strong>{' '}
