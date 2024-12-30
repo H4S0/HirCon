342b47240f37397c6659f9c828ae1dfe9b7e241d
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-12-30 18:16:01 +0100

    Update EducationForm.tsx

--- app/components/forms/EducationForm.tsx
+++ app/components/forms/EducationForm.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { updateEducation } from '@/app/actions';
+import { DeleteEducation, updateEducation } from '@/app/actions';
 import { educationSchema } from '@/app/utils/zodSchemas';
 import { Button } from '@/components/ui/button';
 import {
@@ -20,6 +20,7 @@ import {
 } from '@/components/ui/select';
 import { useForm } from '@conform-to/react';
 import { parseWithZod } from '@conform-to/zod';
+import { format, parse } from 'date-fns';
 import React, { useActionState, useState } from 'react';
 
 interface educationDataProps {
@@ -36,10 +37,12 @@ const EducationForm = ({
   data: educationDataProps;
   educationId: string;
 }) => {
+  const [startDate, setStartDate] = useState(data.startDate);
+  const [endDate, setEndDate] = useState(data.endDate);
+
   const [institution, setInstitution] = useState(data.institution);
   const [degree, setDegree] = useState(data.degree);
-  const [startYear, setStartYear] = useState(data.startDate);
-  const [endYear, setEndYear] = useState(data.endDate);
+
   const [lastResult, action] = useActionState(updateEducation, undefined); // action state
 
   const [form, fields] = useForm({
@@ -87,10 +90,10 @@ const EducationForm = ({
                 id={fields.startYear.name}
                 name={fields.startYear.name}
                 type="date"
-                value={startYear}
+                defaultValue={startDate}
                 required
                 className="mt-1"
-                onChange={(e) => setStartYear(e.target.value)}
+                onChange={(e) => setStartDate(e.target.value)}
                 onKeyDown={(e) => e.key === 'Enter' && e.preventDefault()} // Prevent Enter key from submitting
               />
             </div>
@@ -101,10 +104,10 @@ const EducationForm = ({
                 id={fields.endYear.name}
                 name={fields.endYear.name}
                 type="date"
-                value={endYear}
+                defaultValue={endDate}
                 required
                 className="mt-1"
-                onChange={(e) => setEndYear(e.target.value)}
+                onChange={(e) => setEndDate(e.target.value)}
                 onKeyDown={(e) => e.key === 'Enter' && e.preventDefault()} // Prevent Enter key from submitting
               />
             </div>
@@ -136,9 +139,23 @@ const EducationForm = ({
             </div>
 
             <CardFooter>
-              <Button type="submit" className="w-full">
-                Save
-              </Button>
+              <div className="grid gap-2 w-full mt-5">
+                <Button type="submit" className="w-full">
+                  Save
+                </Button>
+                <Button
+                  type="button"
+                  onClick={async () => {
+                    const formData = new FormData();
+                    formData.append('educationId', educationId);
+
+                    await DeleteEducation(undefined, formData);
+                  }}
+                  className="w-full"
+                >
+                  Delete education
+                </Button>
+              </div>
             </CardFooter>
           </form>
         ) : (
